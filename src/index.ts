397ca93fa69e5bf8963b0e5786c62f12f0dae2c6
markysoft <markharrop@yahoo.com>
2025-02-08 18:08:59 +0000

    remove dist from repo

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono again')
+  return c.text('Hello Hono again!')
 })
 
 app.get('/healthz', (c) => {
   return c.json({ status: 'ok' })
 })
 
-const port = process.env.PORT ? Number(process.env.PORT) : 10000
+const port = process.env.PORT ? Number(process.env.PORT) : 3000
 
 console.log(`Server is running on http://localhost:${port}`)
 
