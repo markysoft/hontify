edfccc729387254ebbaae4e271199d41da378724
markysoft <markharrop@yahoo.com>
2025-02-12 14:16:42 +0000

    interim comit of token refresh

---
+++ src/services/spotify/SpotifyUserToken.ts
@@ -0,0 +1,63 @@
+import { Context } from 'hono'
+import config from '../../config'
+const { spotify } = config
+import { ErrorResponseSchema } from '../common/responses/ErrorResponse'
+import { getCookie, setCookie } from 'hono/cookie'
+import { UserTokenResponseSchema } from '../auth/responses/UserTokenResponse'
+
+const spotifyTokenUrl = 'https://accounts.spotify.com/api/token'
+
+export class SpotifyToken {
+    accessToken = ''
+    refreshToken = ''
+    expires = 0
+    c: Context
+
+    constructor(c: Context) {
+        this.accessToken = getCookie(c, 'accessToken') ?? ''
+        this.refreshToken = getCookie(c, 'refreshToken') ?? ''
+        this.c = c
+    }
+
+    public async getToken() {
+        if (this.accessToken === '') {
+            return await this.updateToken()
+        }
+        return this.accessToken
+
+    }
+
+    private async updateToken(): Promise<void> {
+        try {
+            console.log('updating token')
+            const response = await fetch(spotifyTokenUrl, this.tokenRequest())
+            if (response.status === 200) {
+                const refreshJson = await response.json() as unknown
+                const userToken = UserTokenResponseSchema.parse(refreshJson)
+                this.refreshToken = userToken.refresh_token
+                this.accessToken = userToken.access_token
+                const expires = new Date(Date.now() + ((userToken.expires_in - 10) * 1000))
+                setCookie(this.c, 'accessToken', this.accessToken, { expires, httpOnly: true })
+                setCookie(this.c, 'refreshToken', this.refreshToken)
+            } else {
+                const { error_description } = ErrorResponseSchema.parse(await response.json())
+                throw new Error(`Failed to get token: ${error_description}`)
+            }
+        } catch (e) {
+            console.error(e)
+            throw new Error('Failed to get token')
+        }
+    }
+
+    private tokenRequest(): RequestInit {
+        const authCode = `${spotify.clientId}:${spotify.clientSecret}`
+        return {
+            method: 'POST',
+            body: new URLSearchParams({ 'grant_type': 'refresh_token', 'refresh_token': this.refreshToken }),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + (Buffer.from(authCode).toString('base64')),
+            },
+        }
+    }
+}
