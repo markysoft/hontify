9ac00c7722709c229ab50104617e6a615b9a747a
markysoft <markharrop@yahoo.com>
2025-02-12 13:25:48 +0000

    set current song size as large

--- src/components/spotify/current-song.tsx
+++ src/components/spotify/current-song.tsx
@@ -9,7 +9,7 @@ export const CurrentSong: FC<{ song: CurrentlyPlaying }> = (props: { song: Curre
             <h2 class="title has-text-primary-15">Now Playing</h2>
             <div class="fixed-grid has-1-cols">
                 <div class="grid">
-                    <SongCard song={props.song} size="medium" />
+                    <SongCard song={props.song} size="large" />
                 </div>
             </div>
         </>
